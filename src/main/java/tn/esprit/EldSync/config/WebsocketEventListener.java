8d6aef1d4e594c16e0773a8909d62e43a613aa81
NourAchahlaou <achahlaou.nour@gmail.com>
2024-01-30 22:10:16 +0100

    removed the config

--- src/main/java/tn/esprit/EldSync/config/WebsocketEventListener.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.EldSync.config;
-
-import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-
-public class WebsocketEventListener {
-    public void handleWebSocketConnectListener(SessionDisconnectEvent event) {
-        //to do later
-    }
-}
