2387ba3a907d0e1f1184e62d559af5df3b762174
NourAchahlaou <achahlaou.nour@gmail.com>
2024-01-30 22:05:10 +0100

    configuring websocket

---
+++ src/main/java/tn/esprit/EldSync/config/WebsocketEventListener.java
@@ -0,0 +1,9 @@
+package tn.esprit.EldSync.config;
+
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+public class WebsocketEventListener {
+    public void handleWebSocketConnectListener(SessionDisconnectEvent event) {
+        //to do later
+    }
+}
