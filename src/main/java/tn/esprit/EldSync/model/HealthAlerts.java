71aa70e0e03f57e34c7d83d51481e8b136c40d49
NourAchahlaou <achahlaou.nour@gmail.com>
2024-02-19 23:55:43 +0100

    Added 'HealthAlerts' entity to Health Tracking Management, including enumerations for comprehensive health status tracking.

---
+++ src/main/java/tn/esprit/EldSync/model/HealthAlerts.java
@@ -0,0 +1,26 @@
+package tn.esprit.EldSync.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "healthMetric")
+public class HealthAlerts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Enumerated(EnumType.STRING)
+    private AlertType alertType;
+    private String alertMessage;
+    private LocalDateTime alertDate;
+    @Enumerated(EnumType.STRING)
+    private ResolvedStatus resolvedStatus;
+
+}
