f0908b9c32edc456f3912360cd05fd3e15c3b720
Nour <achahlaou.nour@gmail.com>
2024-05-07 08:47:48 +0100

    made some updates

--- src/main/java/tn/esprit/EldSync/controller/HealthAlertsController.java
+++ src/main/java/tn/esprit/EldSync/controller/HealthAlertsController.java
@@ -1,6 +1,7 @@
 package tn.esprit.EldSync.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/All-health-alerts")
 public class HealthAlertsController {
     private final HealthAlertsService healthAlertsService;
